d08be44745a3ebbab0bde6bd61d95f42dbacc3bb
Donald Lee <51865552+Donald-K-Lee@users.noreply.github.com>
2020-09-05 01:30:58 -0700

    Revived my website's links page

---
+++ linkjs.js
@@ -0,0 +1,25 @@
+var remove1 = document.getElementById("removeaniafter1");
+var remove2 = document.getElementById("removeaniafter2");
+var remove3 = document.getElementById("removeaniafter3");
+var remove4 = document.getElementById("removeaniafter4");
+var remove5 = document.getElementById("removeaniafter5");
+
+window.onload = setTimeout(function(){
+	remove1.classList.remove("ani1");
+}, 300);
+
+window.onload = setTimeout(function(){
+	remove2.classList.remove("ani2");
+}, 600);
+
+window.onload = setTimeout(function(){
+	remove3.classList.remove("ani3");
+}, 900);
+
+window.onload = setTimeout(function(){
+	remove4.classList.remove("ani4");
+}, 1200);
+
+window.onload = setTimeout(function(){
+	remove5.classList.remove("ani5");
+}, 1500);
